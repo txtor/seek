683541ffb3e57118acca52291ac8b60fc4c83492
txtor <txtor@textantrieb.de>
2022-03-10 08:17:35 +0100

    String replaced by &str in filename

--- src/filesearcher.rs
+++ src/filesearcher.rs
@@ -4,24 +4,24 @@ use std::io::{self, BufRead, BufReader};
 pub struct FileSearcher<'a> {
     file :Box<dyn BufRead>,
     query: &'a crate::Query,
-    filename : String,
+    filename : &'a str,
     line_number : u32,
 }
 
-pub struct FileMatch {
-    pub filename : String,
+pub struct FileMatch<'a> {
+    pub filename : &'a str,
     pub line_number : u32,
     pub line : String
 }
 
 impl<'a> FileSearcher<'a> {
-    pub fn new(filename: &str, query: &'a crate::Query) -> crate::SeekResult<Self> {
+    pub fn new(filename: &'a str, query: &'a crate::Query) -> crate::SeekResult<Self> {
         match open(filename) {
             Err(e) => Err(e),
             Ok(file) => Ok(FileSearcher { 
                 file: file,
                 query: query,
-                filename: String::from(filename),
+                filename: filename,
                 line_number: 0
             })
         }
@@ -29,7 +29,7 @@ impl<'a> FileSearcher<'a> {
 }
 
 impl<'a> Iterator for FileSearcher<'a> {
-    type Item = crate::SeekResult<FileMatch>;
+    type Item = crate::SeekResult<FileMatch<'a>>;
     fn next(&mut self) -> Option<Self::Item> {
         let mut lin = String::new();
         match self.file.read_line(&mut lin) {
@@ -46,7 +46,7 @@ impl<'a> Iterator for FileSearcher<'a> {
                 }
                 if found {
                     Some(Ok(FileMatch { 
-                        filename: self.filename.clone(),
+                        filename: self.filename,
                         line_number: self.line_number,
                         line :lin
                     }))
